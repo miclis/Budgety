b3cf07c610d9b0c0072793f3447accbd2d29e43e
miclis <mlgt007@gmail.com>
2019-02-21 14:20:38 +0100

    Init() created

--- app.js
+++ app.js
@@ -26,11 +26,24 @@ var UIController = (function() {
 
 // GLOBAL APP CONTROLLER
 var controller = (function(budgetCtrl, UICtrl) {
-	var DOM = UICtrl.getDOMstrings;
+	// Event listeners
+	var setupEventListeners = function() {
+		var DOM = UICtrl.getDOMstrings();
+
+		document
+			.querySelector(DOM.inputBtn)
+			.addEventListener("click", ctrlAddItem);
+
+		document.addEventListener("keypress", function(event) {
+			if (event.keyCode === 13 || event.which === 13) {
+				ctrlAddItem();
+			}
+		});
+	};
+
 	var ctrlAddItem = function() {
 		// 1. Get the field input data
 		var input = UICtrl.getInput();
-		console.log(input);
 
 		// 2. Add the item to the budget controller
 		// 3. Add the item to the UI
@@ -38,12 +51,11 @@ var controller = (function(budgetCtrl, UICtrl) {
 		// 5. Display the budget on the UI
 	};
 
-	// Event listeners
-	document.querySelector(DOM.inputBtn).addEventListener("click", ctrlAddItem);
-
-	document.addEventListener("keypress", function(event) {
-		if (event.keyCode === 13 || event.which === 13) {
-			ctrlAddItem();
+	return {
+		init: function() {
+			setupEventListeners();
 		}
-	});
+	};
 })(budgetController, UIController);
+
+controller.init();
